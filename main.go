f21763cf580688f8b84934c949d1e962d4fe6c5c
victor <victorreisprog@gmail.com>
2024-02-05 00:06:49 -0300

    feat: mkt cap

--- main.go
+++ main.go
@@ -14,11 +14,17 @@ type CompanyProfile struct {
 	FullTimeEmployees string  `json:"fullTimeEmployees"`
 }
 
+type CompanyMktCap struct {
+	Symbol      string  `json:"symbol"`
+	CompanyName string  `json:"companyName"`
+	MarketCap   float64 `json:"marketCap"`
+	Sector      string  `json:"sector"`
+}
+
 func main() {
 	apiKey := "3sRm5UKmkVsnC0fTvlASpoueSIZCzQTv"
 	reqUrl := fmt.Sprintf("https://financialmodelingprep.com/api/v3/profile/MSFT?apikey=%s", apiKey)
 	resp, err := http.Get(reqUrl)
-	// resp, err := http.Get("https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=3sRm5UKmkVsnC0fTvlASpoueSIZCzQTv")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -39,6 +45,28 @@ func main() {
 		fmt.Printf("Industry: %s\n", company.Industry)
 		fmt.Printf("Full Time Employees: %s\n", company.FullTimeEmployees)
 	}
+
+	mktCap := fmt.Sprintf("https://financialmodelingprep.com/api/v3/stock-screener?apikey=%s", apiKey)
+	resp, err = http.Get(mktCap)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var mktCapResp []CompanyMktCap
+	err = json.NewDecoder(resp.Body).Decode(&mktCapResp)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, company := range mktCapResp {
+		fmt.Printf("Symbol: %s\n", company.Symbol)
+		fmt.Printf("Company Name: %s\n", company.CompanyName)
+		fmt.Printf("Market Cap: %.2f\n", company.MarketCap)
+		fmt.Printf("Sector: %s\n", company.Sector)
+		fmt.Println("--------------------------------------------------")
+	}
+	// fmt.Println(mktCapResp)
 
 	// fmt.Println(jsonResp)
 }
